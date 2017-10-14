2a14537b19ed540cbcb6d2790e2368ea48407e98
Benedek Gagyi <gagyibenedek@yahoo.com>
2017-10-14 22:45:38 +0200

    added a the first 3

--- presentation/index.js
+++ presentation/index.js
@@ -11,19 +11,19 @@ import {
   List,
   Quote,
   Slide,
-  Text
+  Text,
+  S,
+  Link
 } from "spectacle";
 
 // Import image preloader util
 import preloader from "spectacle/lib/utils/preloader";
 
-// Import theme
 import createTheme from "spectacle/lib/themes/default";
 
 // Require CSS
 require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
-
 
 const images = {
   city: require("../assets/city.jpg"),
@@ -35,55 +35,95 @@ const images = {
 preloader(images);
 
 const theme = createTheme({
-  primary: "white",
-  secondary: "#1F2022",
-  tertiary: "#03A9FC",
-  quartenary: "#CECECE"
+  bg: "#dddddd",
+  regular: "#60b200",
+  highlight: "#ef6a00",
+  plain: "#222222"
 }, {
-  primary: "Montserrat",
+  primary: "Rubik",
   secondary: "Helvetica"
 });
+const colors = theme.screen.colors;
+
+const bottomRight = {
+  position: "relative",
+  bottom: "-20px"
+};
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            WebAssembly
-          </Heading>
-          <Heading size={2} fit caps lineHeight={1} textColor="secondary">
-            Taking a look behind the hype
-          </Heading>
-          {/* <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
-          </Text> */}
-        </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
-        </Slide>
-      </Deck>
+    <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
+      {/* ************************************  1  ************************************ */}
+      <Slide transition={["zoom"]} bgColor="bg">
+        <Heading size={1} fit caps lineHeight={1} textColor="highlight">
+          WebAssembly
+        </Heading>
+        <Text margin="10px 0 0" textColor="regular" size={1} fit bold>
+          Taking a look behind the hype
+        </Text>
+        <Text size={5} textAlign="center" style={bottomRight}>
+          <S type="" style={{ color: colors.regular }}>@</S>
+          <S type="" style={{ color: colors.highlight }}>Benedek</S>
+          <S type="" style={{ color: colors.regular }}>Gagyi</S>
+        </Text>
+      </Slide>
+      {/* ************************************  2  ************************************ */}
+      <Slide transition={["fade"]} bgColor="bg">
+        <Heading size={4} textColor="regular">
+          <S type="" style={{ color: colors.regular }}>JavaScript</S>
+          <S type="" style={{ color: colors.highlight }}> performance:</S>
+          <S type="" style={{ color: colors.regular }}> why does it suck?</S>
+        </Heading>
+        <List type="A" textColor="regular">
+          <ListItem>simplicity above everything else</ListItem>
+          <ListItem>dynamic, weakly typed</ListItem>
+          <ListItem>2008: just-in-time (JIT) compiling</ListItem>
+          <ListItem>
+            <Link textColor="highlight" href="https://hacks.mozilla.org/2017/02/a-cartoon-intro-to-webassembly/">
+              For more info, check out Link Clark's work
+            </Link>
+          </ListItem>
+        </List>
+      </Slide>
+      {/* ************************************  3  ************************************ */}
+      <Slide transition={["fade"]} bgColor="bg">
+        <Heading size={4} textColor="regular">
+          <S type="" style={{ color: colors.regular }}>The first step:</S>
+          <S type="" style={{ color: colors.highlight }}> asm</S>
+          <S type="" style={{ color: colors.regular }}>.js</S>
+        </Heading>
+        <List type="A" textColor="regular">
+          <ListItem>2013, Mozilla</ListItem>
+          <ListItem>a subset of JS</ListItem>
+          <ListItem>double vs. int 32</ListItem>
+          <ListItem>i = i|0;</ListItem>
+          <ListItem>
+            <S type="" style={{ color: colors.highlight }}>not </S>
+            a compile target (
+            <S type="" style={{ color: colors.highlight }}>?</S>
+            )
+          </ListItem>
+        </List>
+      </Slide>
+      {/* ************************************  4  ************************************ */}
+      <Slide transition={["fade"]} bgColor="bg" textColor="plain">
+        <Heading size={6} textColor="regular" caps>Standard List</Heading>
+        <List>
+          <ListItem>Item 1</ListItem>
+          <ListItem>Item 2</ListItem>
+          <ListItem>Item 3</ListItem>
+          <ListItem>Item 4</ListItem>
+        </List>
+      </Slide>
+      {/* ************************************  5  ************************************ */}
+      <Slide transition={["fade"]} bgColor="bg" textColor="highlight">
+        <BlockQuote>
+        <Quote>Example Quote</Quote>
+        <Cite>Author</Cite>
+        </BlockQuote>
+      </Slide>
+    </Deck>
     );
   }
 }
